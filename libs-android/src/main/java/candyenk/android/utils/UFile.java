1d826399e7846df205dce616c7c387054438b0b9
Candywzt <candywzt@gmail.com>
2022-10-19 21:26:07 +0800

    重构Java.UFile和Android.UFile,采用多样化的读写流程

--- libs-android/src/main/java/candyenk/android/utils/UFile.java
+++ libs-android/src/main/java/candyenk/android/utils/UFile.java
@@ -1,9 +1,9 @@
 package candyenk.android.utils;
 
 import android.content.Context;
+import androidx.annotation.RawRes;
 
 import java.io.*;
-import java.nio.charset.Charset;
 
 /**
  * Android文件工具
@@ -53,38 +53,26 @@ public class UFile extends candyenk.java.utils.UFile {
         return context.getExternalCacheDir().getAbsolutePath();
     }
 
+
     /**
      * Raw资源文件读取(res/raw/文件夹)
      *
-     * @param raw R.raw.fileName
+     * @param resId R.raw.fileName
      */
-    public static String readRawFile(Context context, int raw, Charset charset) {
-        String str = null;
-        try {
-            InputStream in = context.getResources().openRawResource(raw);
-            str = readParent(in, charset);
-        } catch (IOException e) {
-            //TODO:异常处理
-            e.printStackTrace();
-        }
-        return str;
+    public static InputStream readRaw(Context context, @RawRes int resId) {
+        return context.getResources().openRawResource(resId);
     }
 
     /**
      * Assets资源文件读取(assets/文件夹)
      *
      * @param fileName 文件名带后缀
+     * @return 无法读取返回NULL
      */
-    public static String readAssetsFile(Context context, String fileName, Charset charset) {
-        String str = null;
+    public static InputStream readAssets(Context context, String fileName) {
         try {
-            InputStream in = context.getResources().getAssets().open(fileName);
-            str = readParent(in, charset);
-        } catch (IOException e) {
-            //TODO:异常处理
-            e.printStackTrace();
-        }
-        return str;
+            return context.getResources().getAssets().open(fileName);
+        } catch (IOException e) {return null;}
     }
 
     /**
@@ -93,35 +81,22 @@ public class UFile extends candyenk.java.utils.UFile {
      * @param fileName 文件名带后缀
      * @return
      */
-    public static String readDataFile(Context context, String fileName, Charset charset) {
-        String str = null;
+    public static FileInputStream readData(Context context, String fileName) {
         try {
-            FileInputStream in = context.openFileInput(fileName);
-            str = readParent(in, charset);
-        } catch (IOException e) {
-            //TODO:异常处理
-            e.printStackTrace();
-        }
-        return str;
+            return context.openFileInput(fileName);
+        } catch (IOException e) {return null;}
     }
 
     /**
      * Data文件写入(data/data/包名/files/目录)
      *
      * @param fileName 写入文件名带后缀
-     * @param writestr 写入字符串
-     * @param mode     Content.MODE_PRIVATE覆盖写入;Content.MODE_APPEND追加写入
+     * @param isAppend 是够追加写入
      */
-    public static boolean writeDataFile(Context context, String fileName, String writestr, Charset charset, int mode) {
-        boolean b = false;
+    public static FileOutputStream writeData(Context context, String fileName, boolean isAppend) {
         try {
-            FileOutputStream out = context.openFileOutput(fileName, mode);
-            b = writeParent(out, writestr, charset);
-        } catch (IOException e) {
-            //TODO:异常处理
-            e.printStackTrace();
-        }
-        return b;
+            return context.openFileOutput(fileName, isAppend ? Context.MODE_APPEND : 0);
+        } catch (IOException e) {return null;}
     }
 
     /**
@@ -130,30 +105,20 @@ public class UFile extends candyenk.java.utils.UFile {
      * @param fileName 删除文件名
      * @return 执行结果
      */
-    public static boolean deleteDataFile(Context context, String fileName) {
+    public static boolean deleteData(Context context, String fileName) {
         return context.deleteFile(fileName);
     }
 
     /**
      * Data文件(夹)重命名(data/data/包名/files/目录)
      *
-     * @param oldFilePath 重命名文件路径
-     * @param newFileName 新文件名
-     * @return 返回状态码 -1:文件不存在;0:文件名已存在;1:重命名成功
+     * @param filePath 重命名文件路径
+     * @param newName  新文件名
+     * @return 重命名成功返回true, 其他情况通通返回false
      */
-    public static int renameDataFile(Context context, String oldFilePath, String newFileName) {
-        File oldFile = context.getFileStreamPath(oldFilePath);
-        File newFile = new File(oldFile.getParentFile(), newFileName);
-        if (!oldFile.exists()) {
-            return -1;
-        } else {
-            if (newFile.exists()) {
-                return 0;
-            } else {
-                oldFile.renameTo(newFile);
-                return 1;
-            }
-        }
+    public static boolean renameData(Context context, String filePath, String newName) {
+        File file = context.getFileStreamPath(filePath);
+        return renameFile(file, newName);
     }
 
 }
--- libs-java/src/main/java/candyenk/java/utils/UFile.java
+++ libs-java/src/main/java/candyenk/java/utils/UFile.java
@@ -1,36 +1,26 @@
 package candyenk.java.utils;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import candyenk.java.io.FileType;
+
+import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-import candyenk.java.io.FileType;
-
 /**
  * Java文件工具
- * 文件读取到字符串(可调编码)
- * 文件按行自定义读取(可调编码)
- * 文件按行读取到List
- * 字符串写入文件
- * 文件夹创建
- * 文件(夹)删除与重命名
- * 列出文件夹所有文件(包括子文件夹)
+ * 注意权限问题
+ * <p>
+ * 文件判空
+ * 文件读写String和Bytes(功能超丰富)
+ * 文件(夹)增删
+ * 文件(夹)重命名
+ * 列出所有文件(包括子文件夹)
  * 文件(夹)路径拼接
  * 文件(夹)路径分割
- * 获取当前jar运行路径
+ * 当前jar运行路径
  * 创建缓存文件
  * 获取文件后缀
  * 判断文件类型
@@ -42,6 +32,7 @@ public class UFile {
     /**
      * 文件判空
      * 当前文件路径是否为空或不可访问
+     * 不是文件内容
      */
     public static boolean isEmpty(File file) {
         return file == null || !file.exists();
@@ -52,168 +43,349 @@ public class UFile {
     }
 
     /**
-     * 文件读取
+     * 文件(夹)内容判空
+     * 文件内容为空返回true
+     * 空文件夹返回true
+     * 文件无法读取返回false
      *
-     * @param filePath 文件绝对路径
-     * @return 文件内容
+     * @param file 文件对象
      */
-    public static String readFile(String filePath) {
-        return readFile(filePath, StandardCharsets.UTF_8);
-    }
-
-    public static String readFile(String filePath, Charset charset) {
-        String str = "";
-        try {
-            str = readParent(new FileInputStream(filePath), charset);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return str;
+    public static boolean isEmptyFile(File file) {
+        if (isEmpty(file)) return false;
+        if (file.isDirectory()) return UArrays.isEmpty(file.list());
+        else return file.length() == 0;
     }
 
     /**
-     * 文件按行读取
-     */
-    public static boolean readFileLine(String filePath, Charset charset, Consumer<String> action) throws IOException {
-        String line;
-        BufferedReader reader;
-        if (charset == null) {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-        } else {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), charset));
-        }
-        while ((line = reader.readLine()) != null) {
-            action.accept(line);
-        }
-        if (reader != null) {
-            reader.close();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * 文件读取到列表
-     */
-    public static List<String> readFileList(String filePath, Charset charset) {
-        ArrayList<String> list = new ArrayList<>();
-        try {
-            readFileLine(filePath, charset, s -> list.add(s));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    /**
-     * 文件写入
+     * 文件读取字符串(默认编码)
+     * 底层采用Reader读取String
      *
-     * @param filePath 文件绝对路径
-     * @param content  写入字符串内容
-     * @param append   是否增量写入
-     * @return 写入结果true成功false失败
+     * @param file 输入文件
+     * @return 文件无法读取返回空字符串
      */
-    public static boolean writeFile(String filePath, String content, boolean append) {
-        return writeFile(filePath, content, StandardCharsets.UTF_8, append);
+    public static String readString(File file) {
+        return readString(file, Charset.defaultCharset());
     }
 
-    public static boolean writeFile(String filePath, String content, Charset charset, boolean append) {
+    /**
+     * 文件读取字符串
+     * 底层采用Reader读取String
+     *
+     * @param file    输入文件
+     * @param charset 字符编码
+     * @return 文件无法读取返回空字符串
+     */
+    public static String readString(File file, Charset charset) {
+        return readString(getReader(file, charset));
+    }
+
+    /**
+     * 输入流读取字符串(默认编码)
+     * 底层采用Reader读取String
+     *
+     * @param in 输入流
+     * @return 流无法读取返回空字符串
+     */
+    public static String readString(InputStream in) {
+        return readString(in, Charset.defaultCharset());
+    }
+
+    /**
+     * 输入流读取字符串
+     * 底层采用Reader读取String
+     *
+     * @param in      输入流
+     * @param charset 字符编码
+     * @return 流无法读取返回空字符串
+     */
+    public static String readString(InputStream in, Charset charset) {
+        if (in == null) return "";
+        return readString(new InputStreamReader(in, charset));
+    }
+
+    /**
+     * Reader读取字符串
+     *
+     * @param reader 输入Reader
+     * @return Reader无法读取返回空字符串
+     */
+    public static String readString(Reader reader) {
+        StringBuilder sb = new StringBuilder();
+        return readString(reader, s -> sb.append(s).append("\n")) ? sb.toString() : "";
+    }
+
+    /**
+     * Reader按行读取字符串
+     *
+     * @param reader 输入Reader
+     * @param action 每一行的操作
+     * @return 读取是否成功
+     */
+    public static boolean readString(Reader reader, Consumer<String> action) {
+        if (reader == null) return false;
         boolean result = false;
-        try {
-            result = writeParent(new FileOutputStream(filePath, append), content, charset);
-        } catch (IOException e) {
-            //TODO:异常处理
-            e.printStackTrace();
-        }
+        try (reader; BufferedReader br = reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader)) {
+            String line;
+            while ((line = br.readLine()) != null) action.accept(line);
+            result = true;
+        } catch (IOException ignored) {}
         return result;
     }
 
     /**
-     * 文件夹创建
+     * 文件读取字节数组
+     *
+     * @param file 输入文件
+     * @return 文件无法读取返回空数组
      */
-    public static boolean createFolder(String dirPath) {
-        File dir = new File(dirPath);
-        if (!dir.exists()) {
-            return dir.mkdirs();
+    public static byte[] readBytes(File file) {
+        return readBytes(getInputStream(file));
+    }
+
+
+    /**
+     * 输入流读取字节数组
+     *
+     * @param in 输入流
+     * @return 流无法读取返回空数组
+     */
+    public static byte[] readBytes(InputStream in) {
+        if (in == null) return new byte[0];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (BufferedInputStream bis = in instanceof BufferedInputStream ? (BufferedInputStream) in : new BufferedInputStream(in)) {
+            byte[] b = new byte[1024];
+            int len = 0;
+            while (len != -1) {
+                len = bis.read(b);
+                baos.write(b, 0, len);
+            }
+        } catch (IOException ignored) {}
+        return baos.toByteArray();
+    }
+
+    /**
+     * 字符串写入到文件(默认编码)
+     * 底层采用Writer写入char[]
+     *
+     * @param file     写入文件
+     * @param text     写入内容
+     * @param isAppend 是否增量写入
+     * @return 写入成功与否
+     */
+    public static boolean writeString(File file, String text, boolean isAppend) {
+        return writeString(file, text, Charset.defaultCharset(), isAppend);
+    }
+
+    /**
+     * 字符串写入到文件
+     * 底层采用Writer写入char[]
+     *
+     * @param file     写入文件
+     * @param text     写入内容
+     * @param charset  写入编码
+     * @param isAppend 是否增量写入
+     * @return 写入成功与否
+     */
+    public static boolean writeString(File file, String text, Charset charset, boolean isAppend) {
+        return writeString(getWriter(file, charset, isAppend), text);
+    }
+
+    /**
+     * 字符串写入到流(默认编码)
+     * 底层采用Writer写入char[]
+     *
+     * @param out  写入流
+     * @param text 写入内容
+     * @return 写入成功与否
+     */
+    public static boolean writeString(OutputStream out, String text) {
+        return writeString(out, text, Charset.defaultCharset());
+    }
+
+    /**
+     * 字符串写入到流
+     * 底层采用Writer写入char[]
+     *
+     * @param out     写入流
+     * @param text    写入内容
+     * @param charset 写入编码
+     * @return 写入成功与否
+     */
+    public static boolean writeString(OutputStream out, String text, Charset charset) {
+        if (out == null || text == null) return false;
+        return writeString(new OutputStreamWriter(out, charset), text);
+    }
+
+    /**
+     * 字符串写入到Writer
+     *
+     * @param writer 写入Writer
+     * @param text   写入内容
+     * @return 写入成功与否
+     */
+    public static boolean writeString(Writer writer, String text) {
+        if (writer == null || text == null) return false;
+        try (BufferedWriter bw = writer instanceof BufferedWriter ? (BufferedWriter) writer : new BufferedWriter(writer)) {
+            bw.write(text);
+            bw.close();
+            return true;
+        } catch (IOException ignored) {}
+        return false;
+    }
+
+    /**
+     * 字节数组写入到文件
+     *
+     * @param file     写入文件
+     * @param content  写入内容
+     * @param isAppend 是否增量写入
+     * @return 写入成功与否
+     */
+    public static boolean writeBytes(File file, byte[] content, boolean isAppend) {
+        return writeBytes(getOutputStream(file, isAppend), content);
+    }
+
+    /**
+     * 字节数组写入到流
+     *
+     * @param out     写入流
+     * @param content 写入内容
+     * @return 写入成功与否
+     */
+    public static boolean writeBytes(OutputStream out, byte[] content) {
+        if (out == null || content == null) return false;
+        try (BufferedOutputStream bos = out instanceof BufferedOutputStream ? (BufferedOutputStream) out : new BufferedOutputStream(out)) {
+            bos.write(content);
+            bos.close();
+            return true;
+        } catch (IOException ignored) {}
+        return false;
+    }
+
+
+    /**
+     * 获取文件Reader
+     *
+     * @param file    输入文件
+     * @param charset 文件编码
+     * @return 文件无法读取返回NULL
+     */
+    public static Reader getReader(File file, Charset charset) {
+        if (isEmpty(file)) return null;
+        return new InputStreamReader(getInputStream(file), charset);
+    }
+
+    /**
+     * 获取文件输入流
+     *
+     * @param file 输入文件
+     * @return 文件无法读取返回NULL
+     */
+    public static InputStream getInputStream(File file) {
+        try {return new FileInputStream(file);} catch (FileNotFoundException ignored) {}
+        return null;
+    }
+
+    /**
+     * 获取文件Writer
+     *
+     * @param file    输入文件
+     * @param charset 文件编码
+     * @return 文件无法写入返回NULL
+     */
+    public static Writer getWriter(File file, Charset charset, boolean isAppend) {
+        if (isEmpty(file)) return null;
+        return new OutputStreamWriter(getOutputStream(file, isAppend), charset);
+    }
+
+    /**
+     * 获取文件输出流
+     *
+     * @param file 输出文件
+     * @return 文件无法写入返回NULL
+     */
+    public static OutputStream getOutputStream(File file, boolean isAppend) {
+        try {return new FileOutputStream(file, isAppend);} catch (FileNotFoundException ignored) {}
+        return null;
+    }
+
+
+    /**
+     * 创建文件夹
+     * 自动创建所有父级文件夹
+     * 返回false优肯以及创建了一些文件夹
+     *
+     * @param file 文件对象
+     * @return 创建成功或已存在返回true, 创建失败返回false
+     */
+    public static boolean createFolder(File file) {
+        if (file == null) return false;
+        return file.exists() || file.mkdirs();
+    }
+
+    /**
+     * 文件(夹)删除
+     * 删除所有子文件夹
+     * 返回false也可能删除了一些文件
+     *
+     * @param files 要删除的文件对象们
+     * @return 删除成功或不存在返回true, 删除失败返回false
+     */
+    public static boolean deleteFile(File... files) {
+        if (UArrays.isEmpty(files)) return true;
+        for (File file : files) {
+            if (!file.exists()) return true;
+            if (file.isDirectory() && !deleteFile(file.listFiles())) return false;
+            if (!file.delete()) return false;
         }
         return true;
     }
 
     /**
-     * 文件(夹)删除
-     *
-     * @param filePath 要删除的文件(夹)目录
-     * @return 删除结果
-     */
-    public static boolean deleteFile(String filePath) {
-        File file = new File(filePath);
-        if (file.isDirectory()) {
-            for (File f : file.listFiles()) {
-                f.delete();
-            }
-            return true;
-        } else {
-            return file.delete();
-        }
-    }
-
-    /**
      * 文件(夹)重命名
      *
-     * @param oldFilePath 重命名文件绝对路径
-     * @param newFileName 新文件名
-     * @return 返回状态码 -1:文件不存在;0:文件名已存在;1:重命名成功
+     * @param file    需要重命名的文件对象
+     * @param newName 新的文件名
+     * @return 重命名成功返回true, 其他情况通通返回false
      */
-    public static int renameFile(String oldFilePath, String newFileName) {
-        File oldFile = new File(oldFilePath);
-        File newFile = new File(oldFile.getParent() + "/" + newFileName);
-        if (!oldFile.exists()) {
-            return -1;
-        } else {
-            if (newFile.exists()) {
-                return 0;
-            } else {
-                oldFile.renameTo(newFile);
-                return 1;
-            }
-        }
+    public static boolean renameFile(File file, String newName) {
+        File nf = new File(file.getParent(), newName);
+        if (!file.exists() || nf.exists()) return false;
+        return file.renameTo(nf);
     }
 
     /**
      * 列出文件夹内所有文件
-     * 包括子文件夹
+     * 只列举文件,不列举文件夹
+     * 包括子文件夹文件
+     *
+     * @param file 文件夹对象
+     * @return 失败返回空list
      */
-    public static List<File> listFiles(String filesPath) {
-        final ArrayList<File> fileList = new ArrayList<>();
-        File parentFile = new File(filesPath);
-        String[] fileNames = parentFile.list();
-        if (parentFile.exists() && parentFile.isDirectory() && fileNames != null) {
-            for (String name : fileNames) {
-                File file = new File(filesPath, name);
-                if (file.isDirectory()) {
-                    fileList.addAll(listFiles(file.getAbsolutePath()));
-                } else {
-                    fileList.add(file);
-                }
+    public static List<File> listAllFile(File file) {
+        final ArrayList<File> list = new ArrayList<>();
+        if (isEmpty(file)) return list;
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (!UArrays.isEmpty(files)) {
+                for (File f : files) list.addAll(listAllFile(f));
             }
+        } else {
+            list.add(file);
         }
-        return fileList;
+        return list;
     }
 
     /**
      * 获取文件后缀
+     * 无法获取多重后缀
      */
     public static String getSuffix(String fileName) {
-        if (fileName == null || fileName.trim().length() == 0) {
-            return "";
-        }
+        if (UString.isEmpty(fileName) || fileName.trim().length() == 0) return "";
+        fileName = fileName.trim();
         int index = fileName.lastIndexOf(".");
-        if (index != -1) {
-            return fileName.substring(index + 1);
-        } else {
-            return "";
-        }
+        if (index != -1) return fileName.substring(index + 1);
+        else return "";
     }
 
 
@@ -271,47 +443,6 @@ public class UFile {
         return filePath;
     }
 
-    /**
-     * 文件读取基类
-     */
-    public static String readParent(InputStream in, Charset charset) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        String line;
-        BufferedReader reader;
-        if (charset == null) {
-            reader = new BufferedReader(new InputStreamReader(in));
-        } else {
-            reader = new BufferedReader(new InputStreamReader(in, charset));
-        }
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        if (reader != null) {
-            reader.close();
-            return sb.toString();
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * 文件写入基类
-     */
-    public static boolean writeParent(OutputStream out, String writestr, Charset charset) throws IOException {
-        BufferedWriter write;
-        if (charset == null) {
-            write = new BufferedWriter(new OutputStreamWriter(out));
-        } else {
-            write = new BufferedWriter(new OutputStreamWriter(out, charset));
-        }
-        write.write(writestr);
-        if (write != null) {
-            write.close();
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     /**
      * 创建缓存文件
