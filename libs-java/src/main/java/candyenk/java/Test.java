609dbe85d359d54768f1d22027ed644f79cfad46
CandyKing <candywzt@gmail.com>
2024-09-02 23:49:49 +0800

    一股脑传上去,管它呢反正是自己看的

--- libs-java/src/main/java/candyenk/java/Test.java
+++ libs-java/src/main/java/candyenk/java/Test.java
@@ -2,14 +2,6 @@ package candyenk.java;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        float posX = (float) (1 / 5);
-        float posY = (float) (1 / 6);
-        int x = (int) (1200 * posX);//默认X位置:1/5宽
-        int y = (int) (2550 * posY);//默认Y位置:1/6高
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(posX);
-        System.out.println(posY);
-        System.out.println(1200/5);
+        System.out.println("[你好]");
     }
 }
