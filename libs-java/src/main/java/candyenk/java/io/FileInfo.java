fe70e4716e10eb8679e2c204458bab6308b3167d
CanDyenK <candywzt@gmail.com>
2026-04-03 09:56:45 +0800

    调整FileInfo类构造方法,删除通过File对象构造自定义Info对象的方法 调整静态创建方法

--- libs-java/src/main/java/candyenk/java/io/FileInfo.java
+++ libs-java/src/main/java/candyenk/java/io/FileInfo.java
@@ -2,12 +2,15 @@ package candyenk.java.io;
 
 import candyenk.java.utils.UArrays;
 import candyenk.java.utils.UFile;
+import candyenk.java.utils.UString;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.*;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 文件信息实体类(对File类的包装拓展)
@@ -17,10 +20,8 @@ import java.util.function.Function;
 public class FileInfo implements Comparable<FileInfo> {
     /*************************************静态变量**************************************************/
     protected static final Map<String, FileInfo> customMap = new HashMap<>();//经过自定义的FileInfoMap
-    protected static final Map<String, FileInfo> infoMap = new HashMap<>();//实际存在的FileInfoMap
     public static final FileInfo superInfo = custom("../");//上级文件夹
     public static final FileInfo emptyInfo = custom("File No Found");//空文件夹
-    public static Function<File, File> ffile = file -> file;//修改File的对象
     /*************************************成员变量**************************************************/
     protected File file;//文件源对象
     protected String name;//文件名
@@ -34,45 +35,32 @@ public class FileInfo implements Comparable<FileInfo> {
     /**********************************************************************************************/
     /**
      * 从File对象创建FileInfo对象
-     * 无法到达的地址将变为自定义文件
+     * 文件不存在则变为emptyFileInfo
      */
     public static FileInfo create(File file) {
-        FileInfo info;
-        if (UFile.isEmpty(file)) {
-            info = custom(file);
-        } else {
-            info = infoMap.get(file.getAbsolutePath());
-            if (info == null) {
-                info = new FileInfo(file);
-                infoMap.put(info.path, info);
-            }
-        }
-        return info;
-    }
-
-    /**
-     * 以File为模板创建自定义FileInfo对象
-     * null将创建随机UUID地址自定义FileInfo
-     */
-    public static FileInfo custom(File file) {
-        FileInfo info = null;
-        if (file != null) info = customMap.get(file.getAbsolutePath());
-        if (file == null || info == null) {
-            info = new FileInfo(file);
-            info.setCustom();
-        }
+        if (UFile.isEmpty(file)) return emptyInfo;
+        FileInfo info = new FileInfo();
+        info.file = file;
+        info.path = file.getAbsolutePath();
+        info.name = file.getName();
+        info.type = FileType.type(file);
+        info.size = file.isDirectory() ? 0 : file.length();
+        info.lmd = file.lastModified();
+        info.isHide = info.name.startsWith(".");
         return info;
     }
 
     /**
      * 以自定义字符串创建FileInfo对象
+     * 自定义Info都是文件夹形态
      */
     public static FileInfo custom(String path) {
-        FileInfo info = null;
-        if (path != null || !path.isEmpty()) info = customMap.get(path);
+        if (UString.isEmpty(path)) return emptyInfo;
+        FileInfo info = customMap.get(path);
         if (info == null) {
-            info = new FileInfo(null);
+            info = new FileInfo();
             info.path = info.name = path;
+            info.type = FileType.DIRECTORY;
             info.setCustom();
         }
         return info;
@@ -93,29 +81,13 @@ public class FileInfo implements Comparable<FileInfo> {
         if (UArrays.isEmpty(path)) return new FileInfo[0];
         return UArrays.T2R(path, FileInfo.class, FileInfo::custom);
     }
+/**********************************************************************************************/
+/*************************************构造方法**************************************************/
     /**********************************************************************************************/
-    /*************************************构造方法**************************************************/
-    /**********************************************************************************************/
-    protected FileInfo(File file) {
-        if (file == null) {
-            this.path = this.name = UUID.randomUUID().toString();
-            this.type = FileType.DIRECTORY;
-        } else {
-            file = ffile.apply(file);
-            this.path = file.getAbsolutePath();
-            this.file = file;
-            this.name = file.getName();
-            this.type = UFile.getTypeFile(file);
-            if (file.exists()) {
-                this.size = file.isDirectory() ? 0 : file.length();
-                this.lmd = file.lastModified();
-                this.isHide = this.name.startsWith(".");
-            }
-        }
-    }
-    /**********************************************************************************************/
-    /*************************************继承方法**************************************************/
-    /**********************************************************************************************/
+    protected FileInfo() {}
+/**********************************************************************************************/
+/*************************************继承方法**************************************************/
+/**********************************************************************************************/
     /**
      * 默认排序方案
      * 目录优先
@@ -142,16 +114,9 @@ public class FileInfo implements Comparable<FileInfo> {
     public String toString() {
         return getPath();
     }
-    /**********************************************************************************************/
-    /*************************************公共方法**************************************************/
-    /**********************************************************************************************/
-    /**
-     * 设置文件名
-     */
-    public void setName(String name) {
-        this.name = name;
-        setCustom();
-    }
+/**********************************************************************************************/
+/*************************************公共方法**************************************************/
+/**********************************************************************************************/
 
     /**
      * 设置文件类型
@@ -237,17 +202,22 @@ public class FileInfo implements Comparable<FileInfo> {
 
     /**
      * 获取子文件Info组
+     *
+     * @param showHide 是否显示隐藏文件(夹)
+     * @param showMove 是否显示返回上层
      */
     public FileInfo[] listInfos(boolean showHide, boolean showMove) {
         if (file == null) return new FileInfo[0];
         String[] paths = file.list();
-        int size = UArrays.isEmpty(paths) ? 2 : paths.length + 2;
-        List<FileInfo> list = new ArrayList<>(size);
+        List<FileInfo> list = new ArrayList<>();
         if (showMove && file.getParent() != null) list.add(superInfo);
-        if (size == 2) list.add(emptyInfo);
-        UArrays.forEach(paths, (s) -> {
-            if (showHide || !s.startsWith(".")) list.add(FileInfo.create(new File(file, s)));
-        });
+        if (UArrays.isEmpty(paths)) list.add(emptyInfo);
+        else for (String s : paths) {
+            FileInfo info = FileInfo.create(new File(s));
+            if (info == null || (!showHide && info.isHide)) continue;
+            list.add(info);
+
+        }
         return list.toArray(new FileInfo[0]);
     }
 
@@ -273,11 +243,11 @@ public class FileInfo implements Comparable<FileInfo> {
     public boolean isDirectory() {
         return getType() == FileType.DIRECTORY;
     }
-    /**********************************************************************************************/
-    /*************************************私有方法**************************************************/
+/**********************************************************************************************/
+/*************************************私有方法**************************************************/
     /**********************************************************************************************/
     private void setCustom() {//设置自定义类型
-        infoMap.remove(this.path);
         customMap.put(this.path, this);
+        file = null;
     }
 }
