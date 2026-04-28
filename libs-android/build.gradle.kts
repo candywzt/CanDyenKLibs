d53874d56ab849c3754089a19d2a1a3973cb22f3
CDKWQ <candywzt@gmail.com>
2026-04-28 17:57:52 +0800

    尝试Jstpack Compose

--- libs-android/build.gradle.kts
+++ libs-android/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.language.nativeplatform.internal.Dimensions.libraryVariants
-
 plugins {
     id("com.android.library")
 }
@@ -20,6 +18,7 @@ android {
     buildFeatures {
         buildConfig = true
         aidl = true
+        compose = true
     }
     packaging {
         resources {
@@ -31,19 +30,42 @@ dependencies {
     api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     api(project(":libs-java"))
-    api("androidx.appcompat:appcompat:1.6.1")//AndroidX
-    api("androidx.annotation:annotation:1.9.1")//AndroidX
-    api("androidx.activity:activity:1.8.1")//AndroidX
-    api("androidx.fragment:fragment:1.8.9")//AndroidX
-    api("com.google.android.material:material:1.10.0")//Google MD
-    api("com.squareup.okio:okio:3.17.0")//OkHttp3
-    api("com.squareup.okhttp3:okhttp:5.3.2")//OkHttp3
-    api("com.sun.mail:android-mail:1.6.8")//Mail
-    api("com.sun.mail:android-activation:1.6.8")//Mail
-    api("dev.rikka.shizuku:api:13.1.5")//Sui
-    api("dev.rikka.shizuku:provider:13.1.5")//Shizuku
-    api("de.robv.android.xposed:api:82")//Xposed
-    api("org.lsposed.hiddenapibypass:hiddenapibypass:6.1")//隐藏API调用
+
+    //Android X
+    api("androidx.appcompat:appcompat:1.6.1")
+    api("androidx.annotation:annotation:1.9.1")
+    api("androidx.activity:activity:1.8.1")
+    api("androidx.fragment:fragment:1.8.9")
+
+    //JetPack Compose
+    api("androidx.compose.runtime:runtime:1.11.0")
+    api("androidx.compose.ui:ui:1.11.0")
+    api("androidx.compose.ui:ui-tooling-preview:1.11.0")
+    api("androidx.compose.foundation:foundation:1.11.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.11.0")
+
+    //Google Material
+    api("com.google.android.material:material:1.10.0")
+
+    //OkHttp3
+    api("com.squareup.okio:okio:3.17.0")
+    api("com.squareup.okhttp3:okhttp:5.3.2")
+
+    //Mail
+    api("com.sun.mail:android-mail:1.6.8")
+    api("com.sun.mail:android-activation:1.6.8")
+
+    //Shizuku
+    api("dev.rikka.shizuku:api:13.1.5")
+    api("dev.rikka.shizuku:provider:13.1.5")
+
+    //Xposed
+    api("de.robv.android.xposed:api:82")
+
+    //HideApi
+    api("org.lsposed.hiddenapibypass:hiddenapibypass:6.1")
+
+
 }
 /**
  * 打包Release Aar和源码Jar
