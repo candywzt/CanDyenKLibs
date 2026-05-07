52093c120ddd948cb838210d736a874c1a6f44a3
CDKWQ <candywzt@gmail.com>
2026-05-07 17:36:23 +0800

    更换确认弹窗和提示弹窗的继承链,重整结构和部分方法

--- libs-android/src/main/java/candyenk/android/widget/DialogBottomConfirm.java
+++ libs-android/src/main/java/candyenk/android/widget/DialogBottomConfirm.java
@@ -3,17 +3,24 @@ package candyenk.android.widget;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Gravity;
 import android.view.View;
-import candyenk.android.tools.L;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import candyenk.android.R;
+import candyenk.android.tools.V;
 import candyenk.java.utils.UData;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.util.function.Consumer;
 
 /**
  * 底部弹窗-确认弹窗
  */
-public class DialogBottomConfirm extends DialogBottomTips {
-    private long confirmTime;
+public class DialogBottomConfirm extends DialogBottomView {
+    protected long time;
+    private TextView textView;
     
     public DialogBottomConfirm(Context context) {
         this(context, null);
@@ -25,29 +32,25 @@ public class DialogBottomConfirm extends DialogBottomTips {
     
     protected DialogBottomConfirm(Context context, View view) {
         super(context, view);
-        setTimeOut(0);
+        if (ok) initLayout();
     }
     
     @Override
     public void show() {
         if (!ok || isShowing()) return;
-        if (leftButton.getVisibility() == View.GONE) {
-            L.e(TAG, "尚未设置回调事件");
-            return;
-        }
         super.show();
-        if (isShowing() && confirmTime != 0) {
+        if (isShowing() && time != 0) {
             leftButton.setEnabled(false);
             Handler h = new Handler(Looper.getMainLooper());
             new Thread(() -> {
                 CharSequence text = leftButton.getText();
-                while (isShowing() && confirmTime > 0) {
+                while (isShowing() && time > 0) {
                     try {
                         h.post(() -> {
-                            if (isShowing()) leftButton.setText(UData.L2A(confirmTime / 1000, "s", "%d"));
+                            if (isShowing()) leftButton.setText(UData.L2A(time / 1000, "s", "%d"));
                         });
                         Thread.sleep(1000);
-                        confirmTime -= 1000;
+                        time -= 1000;
                     } catch (Exception ignored) {}
                 }
                 if (isShowing()) {
@@ -80,32 +83,37 @@ public class DialogBottomConfirm extends DialogBottomTips {
     }
     
     /**
-     * @deprecated 不允许使用
+     * 设置图标样式
+     *
+     * @param style 样式ID
      */
-    @Deprecated
-    @Override
-    public void setLeftButton(CharSequence text, Consumer<? extends DialogBottom> leftClick, Consumer<? extends DialogBottom> leftLong) {
-        throw new UnsupportedOperationException("此方法在该类中已被禁用");
+    public void setIconStyle(int style) {
+        //TODO:还没做
     }
     
     /**
-     * @deprecated 不允许使用
+     * 设置内容文本
+     *
+     * @param text 提示文本
      */
-    @Deprecated
-    @Override
-    public void setRightButton(CharSequence text, Consumer<? extends DialogBottom> rightClick, Consumer<? extends DialogBottom> rightLong) {
-        throw new UnsupportedOperationException("此方法在该类中已被禁用");
+    public void setContent(CharSequence text) {
+        if (!ok) return;
+        if (text == null) textView.setVisibility(View.GONE);
+        else {
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(text);
+        }
     }
     
     /**
-     * 设置确认前等待时间
-     * 时间结束前无法点击确认
+     * 设置等待时间
+     * 时间结束前无法继续操作
      * 默认0
      *
      * @param time 等待时间
      */
-    public void setConfirmTime(long time) {
-        if (!isShowing()) confirmTime = time;
+    public void setTime(long time) {
+        if (!isShowing()) this.time = time;
     }
     
     /**
@@ -127,6 +135,25 @@ public class DialogBottomConfirm extends DialogBottomTips {
             }, null);
             super.setOnCancelListener(d -> callback.onNo());
         }
+    }
+    
+    /*** 初始化布局 ***/
+    private void initLayout() {
+        LinearLayout layout = new LinearLayout(context);
+        V.RV(layout).size(-1, -2).orientation(1).paddingDP(20).refresh();
+        
+        LinearLayout l1 = new LinearLayout(context);
+        V.LL(l1).size(-1, -2).orientation(0).gravity(Gravity.CENTER).parent(layout);
+        
+        ImageView iconView = new ImageView(context);
+        V.LL(iconView).sizeDP(80).drawable(R.drawable.ic_ok).parent(l1);
+        
+        TextView titleView = new MaterialTextView(context);
+        V.LL(titleView).size(-1, -1).gravity(Gravity.CENTER).textSize(24).hide().parent(l1);
+        
+        textView = new MaterialTextView(context);
+        V.LL(textView).size(-1, -2).paddingDP(0, 20, 0, 0).hide().parent(layout);
+        super.setContent(layout);
     }
     
     /**
--- libs-android/src/main/java/candyenk/android/widget/DialogBottomTips.java
+++ libs-android/src/main/java/candyenk/android/widget/DialogBottomTips.java
@@ -1,22 +1,15 @@
 package candyenk.android.widget;
 
 import android.content.Context;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import candyenk.android.R;
-import candyenk.android.tools.V;
-import com.google.android.material.textview.MaterialTextView;
+
+import java.util.function.Consumer;
 
 /**
  * 底部弹窗-提示弹窗
  * 3秒后自动关闭
  */
-public class DialogBottomTips extends DialogBottomView {
-    private TextView textView;
-    private long timeout = 3000;
+public class DialogBottomTips extends DialogBottomConfirm {
     
     public DialogBottomTips(Context context) {
         this(context, null);
@@ -28,17 +21,18 @@ public class DialogBottomTips extends DialogBottomView {
     
     protected DialogBottomTips(Context context, View view) {
         super(context, view);
-        if (ok) initLayout();
+        super.time = 3000;
+        super.setCancelable(false, true);
     }
     
     @Override
     public void show() {
         if (!ok || isShowing()) return;
         super.show();
-        if (isShowing() && timeout != 0) {
+        if (isShowing() && time != 0) {
             new Thread(() -> {
                 try {
-                    Thread.sleep(timeout);
+                    Thread.sleep(time);
                 } catch (Exception ignored) {}
                 if (isShowing()) dismiss();
             }).start();
@@ -50,40 +44,17 @@ public class DialogBottomTips extends DialogBottomView {
      */
     @Deprecated
     @Override
-    public <T extends View> T getContent() {
+    public void setLeftButton(CharSequence text, Consumer<? extends DialogBottom> leftClick, Consumer<? extends DialogBottom> leftLong) {
         throw new UnsupportedOperationException("此方法在该类中已被禁用");
     }
     
     /**
-     * @deprecated 请使用 {@link #setContent(CharSequence)}
+     * @deprecated 不允许使用
      */
     @Deprecated
     @Override
-    public void setContent(int viewid) {
+    public void setRightButton(CharSequence text, Consumer<? extends DialogBottom> rightClick, Consumer<? extends DialogBottom> rightLong) {
         throw new UnsupportedOperationException("此方法在该类中已被禁用");
-    }
-    
-    /**
-     * @deprecated 请使用 {@link #setContent(CharSequence)}
-     */
-    @Deprecated
-    @Override
-    public void setContent(View view) {
-        throw new UnsupportedOperationException("此方法在该类中已被禁用");
-    }
-    
-    /**
-     * 设置内容文本
-     *
-     * @param text 提示文本
-     */
-    public void setContent(CharSequence text) {
-        if (!ok) return;
-        if (text == null) textView.setVisibility(View.GONE);
-        else {
-            textView.setVisibility(View.VISIBLE);
-            textView.setText(text);
-        }
     }
     
     /**
@@ -114,44 +85,30 @@ public class DialogBottomTips extends DialogBottomView {
     }
     
     /**
-     * 设置图标样式
-     *
-     * @param style 样式ID
+     * @deprecated 不允许使用
      */
-    public void setIconStyle(int style) {
-        //TODO:还没做
+    @Deprecated
+    @Override
+    public <T extends View> T getContent() {
+        throw new UnsupportedOperationException("此方法在该类中已被禁用");
     }
     
     /**
-     * 设置自动关闭
-     * 在show之前设置,否则无效
-     * 1000-30000之间,或者0
-     *
-     * @param time 关闭时间
+     * @deprecated 请使用 {@link #setContent(CharSequence)}
      */
-    public void setTimeOut(long time) {
-        if (!ok || isShowing()) return;
-        if (time == 0 || (time >= 1000 && time <= 30000)) timeout = time;
+    @Deprecated
+    @Override
+    public void setContent(int viewid) {
+        throw new UnsupportedOperationException("此方法在该类中已被禁用");
     }
     
-    /*** 初始化布局 ***/
-    private void initLayout() {
-        LinearLayout layout = new LinearLayout(context);
-        V.RV(layout).size(-1, -2).orientation(1).paddingDP(20).refresh();
-        
-        LinearLayout l1 = new LinearLayout(context);
-        V.LL(l1).size(-1, -2).orientation(0).gravity(Gravity.CENTER).parent(layout);
-        
-        ImageView iconView = new ImageView(context);
-        V.LL(iconView).sizeDP(80).drawable(R.drawable.ic_ok).parent(l1);
-        
-        TextView titleView = new MaterialTextView(context);
-        V.LL(titleView).size(-1, -1).gravity(Gravity.CENTER).textSize(24).hide().parent(l1);
-        
-        textView = new MaterialTextView(context);
-        V.LL(textView).size(-1, -2).paddingDP(0, 20, 0, 0).hide().parent(layout);
-        super.setContent(layout);
+    /**
+     * @deprecated 请使用 {@link #setContent(CharSequence)}
+     */
+    @Deprecated
+    @Override
+    public void setContent(View view) {
+        throw new UnsupportedOperationException("此方法在该类中已被禁用");
     }
-    
 }
 
